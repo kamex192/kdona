bbf3a0e0cc19c31230b514b1e32c0c0fc23374f2
admin test <admin@test.com>
2020-08-03 08:49:14 +0900

    add sele

--- dona/views.py
+++ dona/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpResponse
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import chromedriver_binary
+
+def get_fx_rate():
+    options = Options()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
+    driver.get('https://info.finance.yahoo.co.jp/fx/')
+    usd_jpy = driver.find_element(By.ID, 'USDJPY_top_bid').text
+    driver.quit()
+    return usd_jpy
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    fx_rate = get_fx_rate()
+    return HttpResponse(fx_rate)
