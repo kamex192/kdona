d687e2e0f092bd184a3c9c89d5cba30cc3454347
admin test <admin@test.com>
2020-08-08 09:48:34 +0900

    add check flag

--- dona/models.py
+++ dona/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_code = models.CharField(max_length=200)
+    item_url = models.CharField(max_length=200)
     item_price = models.CharField(max_length=200)
     is_monitor = models.CharField(max_length=200)
     buy_date = models.DateTimeField('date published')
--- dona/views.py
+++ dona/views.py
@@ -4,16 +4,66 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import chromedriver_binary
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
 
 def get_fx_rate():
     options = Options()
-    options.add_argument('--headless')
+    # options.add_argument('--headless')
     driver = webdriver.Chrome(options=options)
-    driver.get('https://info.finance.yahoo.co.jp/fx/')
-    usd_jpy = driver.find_element(By.ID, 'USDJPY_top_bid').text
+    driver.execute_script(
+        'const newProto = navigator.__proto__;delete newProto.webdriver;navigator.__proto__ = newProto;')
+    driver.get('https://crocro.com/tools/item/view_browser_inf.html')
+    time.sleep(10)
+    purchaseBox = driver.find_element_by_xpath(
+        '//*[@id="purchaseBox"]/div/div/div[2]/form/button')
+    time.sleep(3)
+    purchaseBox.click()
+    time.sleep(10)
     driver.quit()
-    return usd_jpy
+    return 'usd_jpy'
+
 
 def index(request):
     fx_rate = get_fx_rate()
     return HttpResponse(fx_rate)
+
+
+def check(request):
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    driver = webdriver.Chrome(options=options)
+    # driver.execute_script(
+    #     'const newProto = navigator.__proto__;delete newProto.webdriver;navigator.__proto__ = newProto;')
+    driver.get('https://www.google.com')
+    # time.sleep(1)
+    element = driver.find_element(By.CSS_SELECTOR, '[name="q"]')
+    element.send_keys("ブラウザ情報 表示")
+    # time.sleep(1)
+    element.send_keys(Keys.ENTER)
+    # time.sleep(1)
+    selector = 'a'
+    elements = driver.find_elements_by_css_selector(selector)
+    for element in elements:
+        if 'ブラウザ情報表示' in element.text:
+            url = element.get_attribute('href')
+            print(url)
+            element.send_keys(Keys.ENTER)
+            break
+
+    time.sleep(20)
+
+    ret = '自動操縦フラグチェック：'
+
+    selector = 'div.infArea'
+    elements = driver.find_elements_by_css_selector(selector)
+    for element in elements:
+        if 'window.navigator.webdriver' in element.text:
+            if 'true' in element.text:
+                ret = ret + 'ばれてるよー ' + element.text
+                print(ret)
+
+    return HttpResponse(ret)
